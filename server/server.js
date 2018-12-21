06a6bf6ed7efbe87dafb2bd0718e91495eda8ec7
Manish Basnet <manish.basnetnp@gmail.com>
2018-12-21 09:29:08 +0545

    add post /todos route and refactored mongoose

--- server/server.js
+++ server/server.js
@@ -1,64 +1,26 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo',{
-  text: {
-    type: String,
-    required: true,
-    minlenth: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 
-// var newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-//
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo');
-// });
-
-var otherTodo = new Todo({
-  text: 'Something to do'
-});
-
-otherTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-},(err) => {
-  console.log('Unable to save other todo',err);
-});
-
-// User
-// email -require it - trim it- set type - set min length of 1
-var User = mongoose.model('User',{
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlenth: 1
-  }
-});
-
-
-var user = new User({
-  email: 'manish.basnetnp@gmail.com'
-});
-
-user.save().then((doc) => {
-  console.log('User saved',doc)
-},  (e) => {
-console.log('Unable to save user', e);
+app.listen(3002, () => {
+  console.log('Started on port 3002');
 });
